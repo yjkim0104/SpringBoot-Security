7f23e1843af46e95fe527687b03ab715d87af1c6
yjkim <love2468@hanmail.net>
2019-11-14 23:42:12 +0900

    add security dependency and basic configure file

---
+++ src/main/java/com/me/joon/spring/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.me.joon.spring.config;
+
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/me/joon/spring/form/TestController.java
@@ -0,0 +1,43 @@
+package com.me.joon.spring.form;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("msg", "Hello from spring boot");
+		
+		return "index";
+	}
+	
+	@GetMapping("/admin")
+	public String admin(Model model, Principal principal) {
+		if (principal == null) {
+			model.addAttribute("msg", "Hello yea-ryoung");
+		} else {
+			model.addAttribute("msg", "Hello " + principal.getName() + ", you are admin!");
+		}
+		
+		return "admin";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard(Model model, Principal principal) {
+		model.addAttribute("msg", "Hello " + principal.getName());
+		
+		return "dashboard";
+	}
+	
+	@GetMapping("/information")
+	public String information(Model model) {
+		model.addAttribute("msg", "Hinfomationello from spring boot");
+		
+		return "information";
+	}
+}
